bef377d4dce87ae82fc717f623ebcf3642a2013f
Syeda Hafsa <s.hafsa.5124@gmail.com>
2024-05-23 11:50:17 +0400

    contact us css, js

---
+++ contact-page/contact.js
@@ -0,0 +1,12 @@
+//SCROLL EFFECT - ALL PAGES
+window.addEventListener('scroll', function() {
+    var navBar = document.querySelector('.navigationBar');
+    var title = document.getElementById('titlePage');
+    if (window.scrollY > window.innerHeight / 3.5) { // Check if scrolled halfway
+        navBar.classList.add('scrolled');
+        title.classList.add('hidden');
+    } else {
+        navBar.classList.remove('scrolled');
+        title.classList.remove('hidden');
+    }
+});
