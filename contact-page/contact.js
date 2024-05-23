5e033e697d6c9916f71c60452c7250e640c0eb32
Syeda Hafsa <s.hafsa.5124@gmail.com>
2024-05-23 15:31:36 +0400

    contact js

--- contact-page/contact.js
+++ contact-page/contact.js
@@ -1,12 +1,39 @@
 //SCROLL EFFECT - ALL PAGES
 window.addEventListener('scroll', function() {
-    var navBar = document.querySelector('.navigationBar');
-    var title = document.getElementById('titlePage');
-    if (window.scrollY > window.innerHeight / 3.5) { // Check if scrolled halfway
-        navBar.classList.add('scrolled');
-        title.classList.add('hidden');
-    } else {
-        navBar.classList.remove('scrolled');
-        title.classList.remove('hidden');
-    }
+  var navBar = document.querySelector('.navigationBar');
+  var links = document.querySelectorAll('.navigationBar a');
+  var logo = document.querySelector('.navigationBar img');
+  var title = document.getElementById('titlePage');
+
+  if (window.scrollY > window.innerHeight / 6) { // Check if scrolled past a certain point
+      navBar.classList.add('scrolled');
+      title.classList.add('hidden');
+      links.forEach(function(link) {
+          link.classList.add('scrolled-link');
+      });
+      logo.src = "/homepage-images/logo.png"; // Change to the new logo image
+  } else {
+      navBar.classList.remove('scrolled');
+      title.classList.remove('hidden');
+      links.forEach(function(link) {
+          link.classList.remove('scrolled-link');
+      });
+      logo.src = "/blog-page/images/logo.png"; // Change back to the original logo image
+  }
 });
+
+//FAQs section
+var acc = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
